c35582defb84a47f0e7e69a576a290ed90b50106
Michael <kmichael24@gmail.com>
2014-12-30 01:12:09 -0500

    Used XmlPullParser; multi-threading; Looper, Handler, Message, etc.; Sent an HTTP GET request to Flickr's API

---
+++ PhotoGallery/src/com/bignerdranch/android/photogallery/FlickrFetchr.java
@@ -0,0 +1,147 @@
+package com.bignerdranch.android.photogallery;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import android.net.Uri;
+import android.util.Log;
+
+public class FlickrFetchr {
+	public static final String TAG = "FlickrFetchr";
+	
+	private static final String ENDPOINT = "https://api.flickr.com/services/rest/";
+	private static final String API_KEY = "0cbcb0c378b8d5dd3b43be5b6c0232ed";
+	private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
+	private static final String METHOD_SEARCH = "flick.photos.search";
+	private static final String PARAM_EXTRAS = "extras";
+	private static final String PARAM_TEXT = "text";
+	
+	private static final String EXTRA_SMALL_URL = "url_s";
+	
+	private static final String XML_PHOTO = "photo";
+	
+	byte[] getUrlBytes(String urlSpec) throws IOException
+	{
+		URL url = new URL(urlSpec);
+		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+		
+		try
+		{
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			InputStream in = connection.getInputStream();
+			
+			if (connection.getResponseCode() != HttpURLConnection.HTTP_OK)
+			{
+				return null;
+			}
+			
+			int bytesRead = 0;
+			byte[] buffer = new byte[1024];
+			//reads a byte, returns a number between 0 and 255
+			//returns -1 if the end of the stream has been reached, that's why we have "> 0"
+			//stores each byte in the designated array (buffer)
+			while ((bytesRead = in.read(buffer)) > 0)  
+			{
+				out.write(buffer, 0, bytesRead);
+			}
+			out.close();
+			return out.toByteArray();
+		}
+		finally
+		{
+			connection.disconnect();
+		}
+	}
+	
+	public String getUrl(String urlSpec) throws IOException
+	{
+		return new String(getUrlBytes(urlSpec)); //converts a byte array into a string using a default charset
+	}
+	
+	public ArrayList<GalleryItem> downloadGalleryItems(String url)
+	{
+		ArrayList<GalleryItem> items = new ArrayList<GalleryItem>();
+		
+		try
+		{
+			//returns the bytes from the data at this url, into a string of encoded charsets, which
+			//just happens to be in the form of perfect XML
+			String xmlString = getUrl(url);
+			Log.i(TAG, "Receieved xml: " + xmlString);
+			XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+			XmlPullParser parser = factory.newPullParser();
+			parser.setInput(new StringReader(xmlString));
+			
+			parseItems(items, parser);
+		}
+		catch (IOException ioe)
+		{
+			Log.e(TAG, "Failed to fetch items", ioe);
+		}
+		catch (XmlPullParserException xppe)
+		{
+			Log.e(TAG, "Failed to parse items", xppe);
+		}
+		
+		return items;
+	}
+	
+	
+	public ArrayList<GalleryItem> fetchItems()
+	{
+		
+			String url = Uri.parse(ENDPOINT).buildUpon()
+					.appendQueryParameter("method", METHOD_GET_RECENT)
+					.appendQueryParameter("api_key", API_KEY)
+					.appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+					.build().toString();
+			return downloadGalleryItems(url);
+
+	}
+	
+	public ArrayList<GalleryItem> search(String query)
+	{
+		String url = Uri.parse(ENDPOINT).buildUpon()
+				.appendQueryParameter("method", METHOD_SEARCH)
+				.appendQueryParameter("api_key", API_KEY)
+				.appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+				.appendQueryParameter(PARAM_TEXT, query)
+				.build().toString();
+		return downloadGalleryItems(url);
+	}
+	
+	
+	void parseItems(ArrayList<GalleryItem> items, XmlPullParser parser) throws XmlPullParserException, IOException
+	{
+		int eventType = parser.next();
+		
+		while (eventType != XmlPullParser.END_DOCUMENT)
+		{
+			if (eventType == XmlPullParser.START_TAG && XML_PHOTO.equals(parser.getName()))
+			{
+				String id = parser.getAttributeValue(null, "id");
+				String caption = parser.getAttributeValue(null, "title");
+				String smallUrl = parser.getAttributeValue(null, EXTRA_SMALL_URL);
+				
+				GalleryItem item = new GalleryItem();
+				item.setId(id);
+				item.setCaption(caption);
+				item.setUrl(smallUrl);
+				items.add(item);
+				
+				
+				
+			}
+			eventType = parser.next();
+		}
+	}
+}
---
+++ PhotoGallery/src/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,153 @@
+package com.bignerdranch.android.photogallery;
+
+import java.util.ArrayList;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class PhotoGalleryFragment extends Fragment {
+	GridView mGridView;
+	private static final String TAG = "PhotoGalleryFragment";
+	ArrayList<GalleryItem> mItems;
+	ThumbnailDownloader<ImageView> mThumbnailThread;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		
+		setRetainInstance(true);
+		new FetchItemsTask().execute();
+		
+//		mThumbnailThread = new ThumbnailDownloader<ImageView>();
+		mThumbnailThread = new ThumbnailDownloader<ImageView>(new Handler());
+		mThumbnailThread.setListener(new ThumbnailDownloader.Listener<ImageView>() {
+			
+			public void onThumbnailDownloaded(ImageView imageView, Bitmap thumbnail)
+			{
+				if (isVisible())
+				{
+					imageView.setImageBitmap(thumbnail);
+				}
+			}
+		});
+		mThumbnailThread.start();
+		mThumbnailThread.getLooper();
+		Log.i(TAG, "Background thread started");
+		
+	}
+	
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState)
+	{
+		View v = inflater.inflate(R.layout.fragment_photo_gallery, parent);
+		
+		mGridView = (GridView)v.findViewById(R.id.gridView);
+		
+		setupAdapter();
+		
+		return v;
+	}
+	
+	@Override
+	public void onDestroy()
+	{
+		super.onDestroy();
+		mThumbnailThread.quit();
+		Log.i(TAG, "Background thread destroyed");
+	}
+	
+	@Override
+	public void onDestroyView()
+	{
+		super.onDestroyView();
+		mThumbnailThread.clearQueue();
+	}
+	
+	void setupAdapter()
+	{
+		if (getActivity() == null || mGridView == null) return;
+		
+		if (mItems != null)
+		{
+//			mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(), android.R.layout.simple_gallery_item, mItems));
+			mGridView.setAdapter(new GalleryItemAdapter(mItems));
+		}
+		else
+		{
+			mGridView.setAdapter(null);
+		}
+	}
+	
+	//Param, Progress, Result
+	private class FetchItemsTask extends AsyncTask<Void,Void,ArrayList<GalleryItem>>
+	{
+		@Override
+		public ArrayList<GalleryItem> doInBackground(Void... params)
+		{
+//			try
+//			{
+//				String result = new FlickrFetchr().getUrl("http://www.google.com");
+//				Log.i(TAG, "Fetched concents of URL: " + result);
+//			}
+//			catch (IOException ioe)
+//			{
+//				Log.e(TAG, "Failed to fetch URL: ", ioe);
+//			}
+			
+			String query = "android"; //Just for testing
+			
+			if (query != null)
+			{
+				return new FlickrFetchr().search(query);
+			}
+			else
+			{
+				return new FlickrFetchr().fetchItems();
+			}
+			
+		}
+		
+		@Override
+		protected void onPostExecute(ArrayList<GalleryItem> items)
+		{
+			mItems = items;
+			setupAdapter();
+		}
+	}
+	
+	private class GalleryItemAdapter extends ArrayAdapter<GalleryItem>
+	{
+		public GalleryItemAdapter(ArrayList<GalleryItem> items)
+		{
+			super(getActivity(),0,items);
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent)
+		{
+			if (convertView == null)
+			{
+				convertView = getActivity().getLayoutInflater().inflate(R.layout.gallery_item, parent, false);
+			}
+			
+			ImageView imageView = (ImageView)convertView.findViewById(R.id.gallery_item_imageView);
+			imageView.setImageResource(R.drawable.brian_up_close);
+			GalleryItem item = getItem(position);
+			mThumbnailThread.queueThumbnail(imageView, item.getUrl());
+			
+			return convertView;
+		}
+	}
+}
