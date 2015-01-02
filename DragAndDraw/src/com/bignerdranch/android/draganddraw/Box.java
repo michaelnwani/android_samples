9a7c5058ea3287aa5e023be6ec31c41d31d03fb8
Michael <kmichael24@gmail.com>
2015-01-02 18:37:51 -0500

    Played with Canvas and Paint, PointF, Touch events, and creating a custom view

---
+++ DragAndDraw/src/com/bignerdranch/android/draganddraw/Box.java
@@ -0,0 +1,30 @@
+package com.bignerdranch.android.draganddraw;
+
+import android.graphics.PointF;
+
+public class Box {
+	private PointF mOrigin;
+	private PointF mCurrent;
+	
+	public Box(PointF origin)
+	{
+		mOrigin = origin;
+	}
+	
+	public PointF getCurrent()
+	{
+		return mCurrent;
+	}
+	
+	public void setCurrent(PointF current)	
+	{
+		mCurrent = current;
+	}
+	
+	public PointF getOrigin()
+	{
+		return mOrigin;
+	}
+	
+	
+}
